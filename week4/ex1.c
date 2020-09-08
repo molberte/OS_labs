37fb42f76b52bacc9c502c539316da206e1c20da
molberte <molberte@mail.ru>
2020-09-08 19:03:31 +0300

    very 1st task for today

---
+++ week4/ex1.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+int main(){
+  int n = 0;
+  if (fork() == 0){
+    printf("Hello from child [%d - %d]\n",getpid(),n);
+  }else{
+    printf("Hello from parent [%d - %d]\n",getpid(),n);
+  }
+  return 0;
+}
